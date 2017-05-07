ef46850af0d1d388afd8ef949c083c643d1319f2
Mason Raasch <raasch.mason@gmail.com>
2017-05-07 13:14:42 -0700

    added tests for the 'last' function

--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,12 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+  it('returns the whole array if asked for last n === length', () => {
+    expect(_.last(['a', 'b', 'c'], 3)).toEqual(['a', 'b', 'c']);
+  })
+  it('does not modify the original array', () => {
+    const letters = ['a', 'b', 'c'];
+    const lastTwo = _.last(letters, 2);
+    expect(letters).toEqual(['a', 'b', 'c']);
+  });
 });
