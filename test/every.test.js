b0af475a383cd279c0674a291912babf35832ffd
Mason Raasch <raasch.mason@gmail.com>
2017-05-07 22:27:21 -0700

    added test to every for checking bools

--- test/every.test.js
+++ test/every.test.js
@@ -16,6 +16,13 @@ describe('every()', () => {
       const nums = [1, 3, 5, 6, 7];
       expect(_.every(nums, num => num % 2 === 1)).toBe(false);
     });
-
+    it('returns true if we test for booleans being true', () => {
+      const bools = [true, true, true, true];
+      expect(_.every(bools, bool => bool)).toBe(true);
+    });
+    it('returns false if we test for booleans being true and one is false', () => {
+      const bools = [true, false, true, true];
+      expect(_.every(bools, bool => bool)).toBe(false);
+    });
   });
 });
