7274e1b23829a2ff64389c458cdb8a2bf57a9b43
Mason Raasch <raasch.mason@gmail.com>
2017-05-07 18:56:38 -0700

    fixed typo in map test. added addition map test to check for original array modification

--- test/map.test.js
+++ test/map.test.js
@@ -6,9 +6,14 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
-  it('uses the identity function by default' () => {
+  it('uses the identity function by default, resulting in same array', () => {
     const arr = [1, 2, 3, 4, 5];
     const mappedArr = _.map(arr);
-    expect(arr).toEqual(mappedArr); 
+    expect(arr).toEqual(mappedArr);
+  });
+  it('does not modify the original array', () => {
+    const arr = [1,2,3];
+    const mappedArr = _.map(arr, (el) => el * el);
+    expect(arr).toBe(arr);
   });
 });
